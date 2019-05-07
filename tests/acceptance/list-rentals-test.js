b570a52b7e7d25aeb79bd6b0abc5aa00f73faca3
vaskaloidis <vas.kaloidis@gmail.com>
2019-05-07 19:47:20 -0400

    Added static rental data to rentals and displayed on rentals / index

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -26,7 +26,9 @@ module('Acceptance | list rentals', function(hooks) {
     assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
   
-  test('should list available rentals.', async function (assert) {
+  test('should list available rentals.', async function(assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
   
   test('should filter the list of rentals by city.', async function (assert) {
