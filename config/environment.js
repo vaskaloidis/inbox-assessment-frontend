b52960b29fe4246adaf0fe7edb9d9d71baf2a0a0
vaskaloidis <vas.kaloidis@gmail.com>
2019-05-08 18:22:58 -0400

    Disabled mirage, connected frontend to Rails API backend

--- config/environment.js
+++ config/environment.js
@@ -30,7 +30,15 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
-
+  
+  ENV['ember-cli-mirage'] = {
+    enabled: false
+  };
+  
+  ENV.contentSecurityPolicy = {
+    'connect-src': "'self' http://localhost:3000"
+  }
+  
   if (environment === 'test') {
     // Testem prefers this...
     ENV.locationType = 'none';
--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,7 +1,7 @@
 import Service from '@ember/service';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+// import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { resolve } from 'rsvp';
 import {
   click,
@@ -19,7 +19,7 @@ let StubMapsService = Service.extend({
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
-  setupMirage(hooks);
+  // setupMirage(hooks);
   
   hooks.beforeEach(function() {
     this.owner.register('service:map-element', StubMapsService);
