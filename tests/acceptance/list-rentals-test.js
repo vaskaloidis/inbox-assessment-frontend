575ee239f065a0387faa38ccdbd95713794a0d04
vaskaloidis <vas.kaloidis@gmail.com>
2019-05-08 10:51:46 -0400

    Added more advanced API calls to controller and added integration and acceptance testing

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -33,7 +33,12 @@ module('Acceptance | list rentals', function(hooks) {
     assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
   
-  test('should filter the list of rentals by city.', async function (assert) {
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
   
   test('should show details for a selected rental', async function (assert) {
