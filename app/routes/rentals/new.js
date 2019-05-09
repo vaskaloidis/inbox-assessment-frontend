9cf68444ae4873c065de924a57c1d606176bbf93
vaskaloidis <vas.kaloidis@gmail.com>
2019-05-08 20:51:15 -0400

    Removed a component we will not end up using

--- app/routes/rentals/new.js
+++ app/routes/rentals/new.js
@@ -9,19 +9,13 @@ export default Route.extend({
   actions: {
     
     saveRental(newRental) {
-      // newRental.save().then(() => this.transitionTo('rentals'));
-      // const newRecord = this.store.createRecord('rental', formParams);
-  
       newRental.save().then(response => {
         this.set('responseMessage', `Thank you! We saved your rental`);
-        // this.set('title', '');
         this.transitionTo('rentals');
       });
     },
     
     willTransition() {
-      // rollbackAttributes() removes the record from the store
-      // if the model 'isNew'
       this.controller.get('model').rollbackAttributes();
     }
   }
